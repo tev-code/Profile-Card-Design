02c2f53b137f939436d3306bc1e418c1dd9e20a3
Tevin <you@example.com>
2022-10-27 21:08:56 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+/*==== SHOW SOCIAL NETWORKS ====*/
+const showSocial = (toggleCard, socialCard) => {
+  const toggle = document.getElementById(toggleCard),
+  social = document.getElementById(socialCard)
+
+  toggle.addEventListener('click', () => {
+    // if animation class exists, we add the down-animation class
+    if(social.classList.contains('animation')){
+      social.classList.add('down-animation')
+
+      // we remove the down-animation class
+      setTimeout(() => {
+        social.classList.remove('down-animation')
+      }, 1000)
+    }
+    // add the animation class to the div tag
+    social.classList.toggle('animation')
+  })
+}
+showSocial('card-toggle', 'card-social')
